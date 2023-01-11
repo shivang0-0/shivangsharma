0f0ba28fb47220cbc778d6ad70f8e36ed8d5b69b
shivang <shivang.sharma11@pm.me>
2023-01-11 20:21:54 +0530

    home page created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <div className="h-screen w-screen bg-[#171717] fixed inset-0 z-[-1]">
+        <span className="absolute sm:top-[-45%] sm:right-[-10vw] h-[200px] w-full sm:h-[750px] sm:w-[750px] bg-gradient-to-r from-[#50AEF1] to-[#EE5DB4] filter blur-[100px] opacity-20 sm:opacity-30 rounded-full"></span>
+        <span className="absolute bottom-[-2rem] sm:bottom-[-50%] left-[-10vw] h-[200px] w-full sm:h-[750px] sm:w-[750px] bg-gradient-to-r from-[#50AEF1] to-[#EE5DB4] filter blur-[100px] opacity-20 sm:opacity-30 rounded-full"></span>
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
+export default MyApp;
