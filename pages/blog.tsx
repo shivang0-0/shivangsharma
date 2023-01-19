ec9057faa65e7a529bd27b1fca7edd052b74fd7a
shivang <shivang.sharma11@pm.me>
2023-01-19 22:48:53 +0530

    integrated mdx support using next-mdx-remote

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,21 +1,61 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
 import Navbar from "../components/Navbar";
 import Post from "../components/Post";
 
-const Blog = () => {
+interface IPosts {
+  frontMatter: {
+    title: string;
+    cover_image: string;
+    sample_body: string;
+  };
+  slug: string;
+}
+
+interface IProps {
+  posts: IPosts[];
+}
+
+const Blog = ({ posts }: IProps) => {
+  console.log(posts);
   return (
     <>
       <Navbar />
       <div className="container mx-auto w-[80%] md:w-[60%] my-12">
-        <h1 className="text-white font-bold text-2xl">
-          Posts
-        </h1>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <h1 className="text-white font-bold text-2xl">Posts</h1>
+        {posts?.map((post) => (
+          <Post post={post} key={post.slug} />
+        ))}
       </div>
     </>
   );
 };
 
 export default Blog;
+
+export async function getStaticProps() {
+  let files = fs.readdirSync("pages/posts");
+
+  // Filter out only mdx files
+  files = files.filter((file) => file.split(".")[1] === "mdx");
+
+  const posts = await Promise.all(
+    files.map((file) => {
+      const mdData = fs.readFileSync(path.join("pages/posts", file), "utf-8");
+
+      const { data: frontMatter } = matter(mdData);
+
+      return {
+        frontMatter,
+        slug: file.split(".")[0],
+      };
+    })
+  );
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,96 @@
+import fs from "fs";
+import path from "path";
+import matter, { language } from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import styles from "../../styles/Post.module.css";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/nightOwl";
+
+interface IProps {
+  frontMatter: {
+    title: string;
+    cover_image: string;
+    sample_body: string;
+  };
+  mdxSource: MDXRemoteSerializeResult<Record<string, unknown>>;
+}
+
+const Post = ({ frontMatter, mdxSource }: IProps) => {
+  const components = {
+    code: function code({ className, ...props }) {
+      return (
+        <Highlight
+          {...defaultProps}
+          code={props.children}
+          language={`${className.split("-")[1]}` as any}
+          theme={theme}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+      );
+    },
+  };
+
+  return (
+    <div className={styles.post__container}>
+      <img
+        src={frontMatter.cover_image}
+        alt={frontMatter.title}
+        className="rounded-md mt-8"
+      />
+      <h1 className="text-4xl text-white font-bold mt-6 mb-12">
+        {frontMatter.title}
+      </h1>
+      <MDXRemote {...mdxSource} />
+    </div>
+  );
+};
+
+export default Post;
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join("pages/posts"));
+
+  //Generate path for each MDX files
+  const paths = files.map((file) => {
+    return {
+      params: {
+        slug: file.replace(".mdx", ""),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const markdown = fs.readFileSync(
+    path.join("pages/posts", slug + ".mdx"),
+    "utf-8"
+  );
+
+  const { data: frontMatter, content } = matter(markdown);
+
+  const mdxSource = await serialize(content);
+
+  return {
+    props: {
+      frontMatter,
+      mdxSource,
+    },
+  };
+}
